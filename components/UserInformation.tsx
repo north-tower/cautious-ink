d78ce79aede806c5b0200c0fe8f95cc842284ef0
north-tower <muneshmyke@gmail.com>
2024-06-27 18:00:14 +0300

    14

--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -1,17 +1,39 @@
 import { currentUser } from '@clerk/nextjs/server'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { SignedIn } from '@clerk/nextjs'
 
 
 async function UserInformation() {
-    const user = await currentUser()
+    const user = await currentUser();
+
+    const firstName = user?.firstName;
+    const lastName = user?.lastName;
+    
   return (
     <div>
         <Avatar>
-            <AvatarImage src={user?.imageUrl || "https://github.com/shadcn.png"} />
+            {user?.id ? (
+                <AvatarImage src={user?.imageUrl} />
+
+            ) : (
+                <AvatarImage src="https://github.com/shadcn.png" />
+            )}
             <AvatarFallback>{user?.firstName?.charAt(0)}
                 {user?.lastName?.charAt(0)}
             </AvatarFallback>
         </Avatar>
+
+        <SignedIn>
+            <div className='text-center'>
+                <p className='font-semibold'>
+                    {firstName} {lastName}
+                </p>
+
+                <p className='text-xs'>
+                    @{firstName} {lastName}-{user?.id?.slice(-4)}
+                </p>
+            </div>
+        </SignedIn>
     </div>
   )
 }
