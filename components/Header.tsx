648c00da65ac185b486778c62394c975352e05b1
north-tower <muneshmyke@gmail.com>
2024-06-27 15:48:29 +0300

    2

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Image from "next/image"
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="flex">
+      <Image className="rounded-lg"
+      src="https://links.papareact.com/b3z"
+      width={40}
+      height={40}
+      alt="logo" />
+    </div>
   )
 }
 
