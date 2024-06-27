2d294e2668e8520537309f57f00d44a4d7ed32c2
north-tower <muneshmyke@gmail.com>
2024-06-27 17:18:09 +0300

    11

---
+++ components/UserInformation.tsx
@@ -0,0 +1,17 @@
+import { currentUser } from '@clerk/nextjs/server'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+async function UserInformation() {
+    const user = await currentUser()
+  return (
+    <div>
+        <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+    </div>
+  )
+}
+
+export default UserInformation
