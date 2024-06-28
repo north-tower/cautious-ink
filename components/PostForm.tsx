e21d76b5cac93975cb107c6ab6f1293f6d43c039
north-tower <muneshmyke@gmail.com>
2024-06-28 10:10:30 +0300

    20

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useUser } from "@clerk/nextjs";
 
 function PostForm() {
+
+    const { user } = useUser();
   return (
-    <div>PostForm</div>
+    <div>
+        <form action="">
+            <div>
+            <Avatar>
+           
+                <AvatarImage src={user?.imageUrl} />
+
+                <AvatarFallback>{user?.firstName?.charAt(0)}
+                    {user?.lastName?.charAt(0)}
+                </AvatarFallback>
+            </Avatar>
+
+            <input className="flex-1 outline-none rounded-full py-3 px-4 border"
+            type="text" name="postInput" placeholder="Start writing a post" />
+
+            </div>
+        </form>
+    </div>
   )
 }
 
