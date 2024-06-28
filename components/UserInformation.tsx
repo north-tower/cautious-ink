2dd9eb0df2824759998c93f7da5c7cf867199bb0
north-tower <muneshmyke@gmail.com>
2024-06-28 09:43:40 +0300

    17

--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -12,7 +12,8 @@ async function UserInformation() {
     const imageUrl = user?.imageUrl;
     
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center
+    bg-white mr-6 rounded-lg border py-4'>
         <Avatar>
             {user?.id ? (
                 <AvatarImage src={imageUrl} />
@@ -51,6 +52,15 @@ async function UserInformation() {
         </SignedOut>
 
         <hr className='w-full border-gray-200 my-5' />
+
+        <div className='flex justify-between w-full px-4 text-sm'>
+            <p className='font-semibold text-gray-400'>Posts</p>
+            <p className='text-blue-400'>0</p>
+        </div>
+        <div className='flex justify-between w-full px-4 text-sm'>
+            <p className='font-semibold text-gray-400'>Comments</p>
+            <p className='text-blue-400'>0</p>
+        </div>
     </div>
   )
 }
