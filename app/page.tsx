6eb6370be43f5679951b0dba7676a4b2cddaeda7
north-tower <muneshmyke@gmail.com>
2024-06-28 10:00:35 +0300

    19

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import PostForm from "@/components/PostForm";
 import UserInformation from "@/components/UserInformation";
 import Image from "next/image";
 
@@ -10,11 +11,11 @@ export default function Home() {
       </section>
 
       <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
-
+        <PostForm />
       </section>
 
       <section className="hidden xl:inline justify-center col-span-2">
-        
+
       </section>
 
     </div>
