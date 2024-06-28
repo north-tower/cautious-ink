c62686c6341f9e9a0caf5c5cd6f159658f356060
north-tower <muneshmyke@gmail.com>
2024-06-28 10:23:47 +0300

    21

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -1,13 +1,21 @@
+'use client'
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useUser } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { ImageIcon } from "lucide-react";
+import { useRef } from "react";
 
 function PostForm() {
+    const ref = useRef<HTMLFormElement>(null);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
 
     const { user } = useUser();
   return (
     <div>
-        <form action="">
-            <div>
+        <form action="" ref={ref} >
+            <div className="flex items-center space-x-2"> 
             <Avatar>
            
                 <AvatarImage src={user?.imageUrl} />
@@ -17,9 +25,22 @@ function PostForm() {
                 </AvatarFallback>
             </Avatar>
 
-            <input className="flex-1 outline-none rounded-full py-3 px-4 border"
+            <input className="flex-1 outline-none rounded-full py-3 px-4 border" ref={fileInputRef}
             type="text" name="postInput" placeholder="Start writing a post" />
 
+            <input type="file" name="image" accept="image/*" hidden />
+
+            <button type="submit" hidden>
+                Post
+            </button>
+
+            </div>
+
+            <div>
+                <Button>
+                    <ImageIcon className="mr-2" size={16} color="currentColor" />
+                    Add
+                </Button>
             </div>
         </form>
     </div>
